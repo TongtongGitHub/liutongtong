025d01010af43eb41c3774208f326c0c80d29d38
unknown <liutongtong@focuschina.com>
2017-02-08 15:40:57 +0800

    structure

--- js/index.js
+++ js/index.js
@@ -1,77 +1,8 @@
 ;(function($){
-    var fs2 = new Slider({
-        naturalOrder:0,
-        interval:3000,
-        speed:1000,
-        autoPlay:false,
-        animationType:'fade',
-        carrier:{
-            content: ".J-home-slider .J-slider-list",
-            banner: ".J-home-slider .J-slider-control"
-        },
-        // extend:{
-        //     switchSpot:function(outObj, inObj, motionDir, motionTime, callback) {
-        //         outObj.css({
-        //             'position' : 'absolute',
-        //             'zIndex' : 2
-        //         });
-        //         inObj.css({
-        //             'position' : 'absolute',
-        //             'zIndex' : 1
-        //         }).show();
-        //         outObj.fadeOut(motionTime / 2, function () {
-        //             outObj.css({
-        //                 'position' : 'static',
-        //                 'zIndex' : 'auto'
-        //             });
-        //             inObj.css({
-        //                 'position' : 'static',
-        //                 'zIndex' : 'auto'
-        //             });
-        //             callback.call(this);
-        //         });
-        //     }
-        // }
+
+    //header
+    $(".J-bar").on('click', function(event) {
+        $(".J-nav").toggleClass('nav-show');
     });
-
-//     var flag = false;
-//     function waitprocess(slider,index){
-//         var list = $(slider.config.carrier.banner).children('li');
-//         list.stop();
-//         list.removeAttr('style');
-//         var timer = list.eq(index);
-//         timer.animate({ 'backgroundPositionX' : '0'}, slider.config.interval, function() {
-//             timer.removeAttr('style');
-//         });
-//     }
-//     waitprocess(fs2,0);
-
-//     fs2.onSpotCompleted(function(e, index, m) {
-//         if (!flag) {
-//             waitprocess(fs2,index);
-//         }
-//     });
-
-// // 动画停止清除未执行完的计时器
-//     $('.J-home-slider').hover(function() {
-//         flag = true;
-//         var list = $(fs2.config.carrier.banner).children('li');
-//         list.stop();
-//         list.removeAttr('style');
-//     }, function(){
-//         flag = false;
-//         var timer = $(fs2.config.carrier.banner).children('.selected');
-//         timer && timer.animate({ 'backgroundPositionX' : '0'}, fs2.config.interval, function() {
-//             timer.removeAttr('style');
-//         });
-//     });
-
-//     $('.slider .pre').bind('click', function() {
-//         fs2.prev();
-//     });
-
-//     $('.slider .next').bind('click', function() {
-//         fs2.next();
-//     });
 
 })(jQuery)
