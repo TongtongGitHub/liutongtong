db80bb008f228e1549ff730a493e2ce62676d3da
unknown <liutongtong@focuschina.com>
2017-03-23 16:42:40 +0800

    test

--- js/index.js
+++ js/index.js
@@ -5,4 +5,15 @@
         $(".J-nav").toggleClass('nav-show');
     });
 
+    $(".desc h1").addClass('animate-fadeUp');
+
+    $(window).scroll(function(event){
+    	var pos1 = $(window).scrollTop();//文档上端隐藏内容的高度
+    	var pos2 = $(".work").offset().top;//文档特定元素距离顶端的高度
+    	console.log(pos2 - pos1); //元素距离工作区顶端的高度
+    });
+
+    $(".scroll-top").on("click",function(){
+    	$(window).scrollTop(0);
+    });
 })(jQuery)
